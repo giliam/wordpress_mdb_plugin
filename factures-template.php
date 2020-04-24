d18d181ccd9062ab6841e2e59629153a1eab4dbf
giliam <matthieu.denoux@gmail.com>
2020-04-24 13:52:33 +0200

    Fixes on display issues and adds sidebar

--- factures-template.php
+++ factures-template.php
@@ -36,65 +36,73 @@ foreach ($query_invoices as $key => $invoice) {
 }
 get_header();
 ?>
+<section id="primary" class="content-area">
+    <main id="main" class="site-main" role="main">
 
-<article class="post-2 page type-page status-publish hentry" id="post-2">
-    <header class="entry-header has-text-align-center header-footer-group">
-        <div class="entry-header-inner section-inner medium">
-            <h2>Vos factures</h2>
+        <header class="page-header">
+            <h1 class="page-title">Vos factures</h1>
+        </header><!-- .page-header -->
+        <div class="page-content">
+            <div data-elementor-type="wp-post" data-elementor-settings="[]">
+                <div class="elementor-inner">
+                    <div class="elementor-section-wrap">
+                        <?php
+                        foreach ($invoices as $key => $invoice) {
+                        ?>
+                            <h4>
+                                Commande du <?php $date_ope = DateTime::createFromFormat("Y-m-d 00:00:00", $invoice["date_ope"]);
+                                            echo $date_ope->format("d m Y"); ?>
+                            </h4>
+                            <p>Vous avez acheté <?php echo count($invoice["operations"]); ?> produits pour un total de <?php echo number_format($invoice["total"], 2); ?>€</p>
+                            <p onClick="hide_invoice('invoice<?php echo $key; ?>')" style="font-size:0.8em; padding-left: 10px; text-decoration: underline;"><em>... Cliquez ici pour voir le détail</em></p>
+                            <table id="invoice<?php echo $key; ?>" style="display:none">
+                                <tr>
+                                    <th>Produit</th>
+                                    <th>Quantité</th>
+                                    <th>Prix</th>
+                                    <th>Total</th>
+                                </tr>
+                                <?php
+                                foreach ($invoice["operations"] as $key => $ope) {
+                                ?>
+                                    <tr>
+                                        <td><?php echo $ope->produit; ?></td>
+                                        <td><?php echo $ope->quantite; ?></td>
+                                        <td><?php echo $ope->prix; ?></td>
+                                        <td><?php echo $ope->quantite * $ope->prix; ?></td>
+                                    </tr>
+                                <?php
+                                }
+                                ?>
+                            </table>
+                        <?php
+                        }
+
+                        if (empty($invoices)) {
+                            echo "<p>Aucune commande effectuée pour l'instant ou présente dans la base de données.</p>";
+                        }
+                        ?>
+                    </div><!-- .page-content -->
+                </div><!-- .page-content -->
+            </div><!-- .page-content -->
+
         </div>
-    </header>
-    <div class="post-inner thin ">
+        <footer class="page-footer">
+        </footer><!-- .page-footer -->
 
-        <div class="entry-content">
-            <?php
-            foreach ($invoices as $key => $invoice) {
-            ?>
-                <h4>
-                    Commande du <?php $date_ope = DateTime::createFromFormat("Y-m-d 00:00:00", $invoice["date_ope"]);
-                                echo $date_ope->format("d m Y"); ?>
-                </h4>
-                <p>Vous avez acheté <?php echo count($invoice["operations"]); ?> produits pour un total de <?php echo number_format($invoice["total"], 2); ?>€</p>
-                <p onClick="hide_invoice('invoice<?php echo $key; ?>')" style="font-size:0.8em; padding-left: 10px; text-decoration: underline;"><em>... Cliquez ici pour voir le détail</em></p>
-                <table id="invoice<?php echo $key; ?>" style="display:none">
-                    <tr>
-                        <th>Produit</th>
-                        <th>Quantité</th>
-                        <th>Prix</th>
-                        <th>Total</th>
-                    </tr>
-                    <?php
-                    foreach ($invoice["operations"] as $key => $ope) {
-                    ?>
-                        <tr>
-                            <td><?php echo $ope->produit; ?></td>
-                            <td><?php echo $ope->quantite; ?></td>
-                            <td><?php echo $ope->prix; ?></td>
-                            <td><?php echo $ope->quantite * $ope->prix; ?></td>
-                        </tr>
-                    <?php
-                    }
-                    ?>
-                </table>
-            <?php
-            }
+    </main><!-- #main -->
 
-            if (empty($invoices)) {
-                echo "<p>Aucune commande effectuée pour l'instant ou présente dans la base de données.</p>";
-            }
-            ?>
-        </div><!-- .entry-content -->
-
-    </div>
-</article>
+</section>
 <script type="text/javascript">
     function hide_invoice(invoiceId) {
         var x = document.getElementById(invoiceId);
         if (x.style.display === "none") {
-            x.style.display = "block";
+            x.style.display = "initial";
         } else {
             x.style.display = "none";
         }
     }
 </script>
+<?php get_sidebar(); ?>
 <?php
 get_footer();
